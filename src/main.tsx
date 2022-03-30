63ce8f90eb0df9ba0bce0dee1cd5e03734c98301
Chris4496 <62294842+Chris4496@users.noreply.github.com>
2022-03-30 13:19:33 +0800

    upgraded to react18

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { registerSW } from "virtual:pwa-register";
 
 registerSW({ immediate: true });
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <ChakraProvider>
       <ColorModeScript initialColorMode="dark" />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );

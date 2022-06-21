ac0eee57cf5e6529fdf257535daec473a44610f8
Chris4496 <62294842+Chris4496@users.noreply.github.com>
2022-06-21 19:56:20 +0800

    Finally fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { registerSW } from "virtual:pwa-register";
@@ -7,12 +7,12 @@ import { registerSW } from "virtual:pwa-register";
 registerSW({ immediate: true });
 
 const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(
+render(
   <React.StrictMode>
     <ChakraProvider>
       <ColorModeScript initialColorMode="dark" />
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  container
 );
